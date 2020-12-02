2a697e26435c3a5372e5b5eeb88f462d2134e9e0
aasimmd <aasim.md00@gmail.com>
2020-12-02 23:46:33 +0530

    remove hooks and add redux to homescreen

--- frontend/src/pages/HomeScreen.js
+++ frontend/src/pages/HomeScreen.js
@@ -1,38 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+//import axios from 'axios'; and removing useState
 import Product from '../components/Product';
 import MessageBox from '../components/MessageBox';
 import LoadingBox from '../components/LoadingBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
-    //react hook : default value of products is [] | to change state use setproduct
-    const [products, setProducts] = useState([]);
+    // REMOVING REACT HOOKS and using REDUX
+    // //react hook : default value of products is [] | to change state use setproduct
+    // const [products, setProducts] = useState([]);
 
-    //loading
-    const [loading, setLoading] = useState(false);
+    // //loading
+    // const [loading, setLoading] = useState(false);
 
-    //errors
-    const [error, setError] = useState(false);
+    // //errors
+    // const [error, setError] = useState(false);
 
     //component didmount executes only ones | handler , dependencies
+
+    const dispatch = useDispatch();
+    const productList = useSelector(state => state.productList);
+    const {loading, error, products} = productList;
+
     useEffect(()=>{
-        //ajax request
-        const fetchData = async ()=>{
-            try
-            {
-                setLoading(true);
-                const {data} = await axios.get('/api/products');
-                setLoading(false);
-                setProducts(data);
-            }
-            catch(err)
-            {
-                setError(err.message);
-                setLoading(false);
-            }
+        // //ajax request
+        // const fetchData = async ()=>{
+        //     try
+        //     {
+        //         setLoading(true);
+        //         const {data} = await axios.get('/api/products');
+        //         setLoading(false);
+        //         setProducts(data);
+        //     }
+        //     catch(err)
+        //     {
+        //         setError(err.message);
+        //         setLoading(false);
+        //     }
             
-        };
-        fetchData();
+        // };
+        // fetchData();
+
+        dispatch(listProducts());
+
     }, []);
     
     return (
