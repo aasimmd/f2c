fd469aae4e1d1fedb0631cc5848e3c984b17d3f1
aasimmd <aasim.md00@gmail.com>
2020-12-06 23:24:24 +0530

    signin screen and implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import {BrowserRouter, Link, Route} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
 import CartScreen from './pages/CartScreen';
 import HomeScreen from './pages/HomeScreen';
 import ProductScreen from './pages/ProductScreen';
+import SigninScreen from './pages/SigninScreen';
 
 function App() {
 
-    const cart = useSelector(state => state.cart);
+    const cart = useSelector((state) => state.cart);
     const {cartItems} = cart;
+
+    const userSignin = useSelector((state) => state.userSignin);
+    const {userInfo} = userSignin;
+
+    const dispatch = useDispatch();
+
+    const signoutHandler = () => {
+        dispatch(signout());
+    }
 
     return (
     <BrowserRouter>
@@ -29,12 +40,27 @@ function App() {
                     )
                 }
                 </Link>
-                <Link to="/signin" >Sign In</Link>
+                {
+                    userInfo
+                    ? (
+                        <div className="dropdown">
+                            <Link to="#">{userInfo.name}<i className="fa fa-caret-down"></i>{' '}</Link>
+                            <ul className="dropdown-content">
+                                <Link to="#" onClick={signoutHandler}>Sign Out</Link>
+                            </ul>
+                        </div>
+                    )
+                    : (
+                        <Link to="/signin" >Sign In</Link>
+                    )
+                }
+                
             </div>
         </header>
 
         <main>
 
+            <Route path="/signin" component={SigninScreen}></Route>
             <Route path="/cart/:id?" component={CartScreen}></Route>
             <Route path="/product/:id" component={ProductScreen}></Route>
             <Route path="/" component={HomeScreen} exact></Route>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,15 @@ import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { userSigninReducer } from './reducers/userReducers';
 
-const initialState = { cart : {
+const initialState = { 
+    userSignin : {
+        userInfo: localStorage.getItem('userInfo')
+        ? JSON.parse(localStorage.getItem('userInfo'))
+        : null,
+    }, 
+    cart : {
         cartItems: localStorage.getItem('cartItems')
         ? JSON.parse(localStorage.getItem('cartItems'))
         : [],
@@ -13,7 +20,8 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userSignin : userSigninReducer
 })
 
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
